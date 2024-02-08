95c8d2fdbe6ca31494f5f6c711cfd29db0abb8d6
ekin <ekinkarakoc1@gmail.com>
2024-02-08 05:12:12 +0300

    "second commit"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        System.out.println("Matematik değerini giriniz :");
+
+        mat = scan.nextInt();
+
+        System.out.println("Fizik değerini giriniz :");
+
+        fizik = scan.nextInt();
+        System.out.println("Kimya değerini giriniz :");
+
+        kimya = scan.nextInt();
+        System.out.println("Turkce değerini giriniz :");
+
+        turkce = scan.nextInt();
+        System.out.println("Tarih değerini giriniz :");
+
+        tarih = scan.nextInt();
+        System.out.println("Muzik değerini giriniz :");
+
+        muzik = scan.nextInt();
+
+        double toplam = (mat+fizik+kimya+tarih+turkce+muzik);
+
+        double sonuc = toplam / 6;
+
+        System.out.println(sonuc);
+
     }
 }
